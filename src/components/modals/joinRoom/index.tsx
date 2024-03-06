c9a770d581afea3639e8a8a5fea684e889496dc6
SkyLuke11 <sky.lucas003@gmail.com>
2024-03-06 19:20:14 -0300

    teste

--- src/components/modals/joinRoom/index.tsx
+++ src/components/modals/joinRoom/index.tsx
@@ -1,67 +1,63 @@
-import { FormEvent, useEffect, useState } from 'react';
-import { Input } from '../../input';
-import { Button, Container, Footer } from './styles'
-// import { api } from '../../../services/api';
-import { toast } from 'react-toastify';
-import { IoLockClosedOutline } from "react-icons/io5";
-import { useNavigate } from 'react-router-dom';
-import { InputOTP } from '../../inputOTP';
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { BackError } from "../../../types/error-back";
+import { InputOTP } from "../../inputOTP";
+import { Button, Container, Footer } from "./styles";
 
 interface Type {
-  [key: number]: string
+  [key: number]: string;
 }
 
 export function ModalJoinRoom({ setClose }: { setClose: () => void }) {
-
   const [inputs, setInputs] = useState<Type>({
-    0: '',
-    1: '',
-    2: '',
-    3: '',
-    4: '',
-    5: '',
-  })
+    0: "",
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+  });
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const createCategory = useCallback(
+    function (code: string) {
+      try {
+        // const response = await api.post('/categoria', {nome, email})
+        // console.log(response.data)
+
+        navigate(`/room/${code}`);
+      } catch (e) {
+        const error = e as BackError;
+        toast.error(error.message);
+        console.log(error);
+      }
+    },
+    [navigate]
+  );
 
   useEffect(() => {
+    const code = Object.values(inputs);
 
-    const code = Object.values(inputs)
-
-    if (code.join('').length == 6) {
-      createCategory(code.join(''))
+    if (code.join("").length == 6) {
+      createCategory(code.join(""));
     }
-
-  }, [inputs])
-
-  async function createCategory(code: string) {
-
-    try {
-
-      // const response = await api.post('/categoria', {nome, email})
-      // console.log(response.data)
-
-      navigate(`/room/${code}`)
-
-    } catch (e: any) {
-      toast.error(e.response.data.msg)
-      console.log(e.response)
-    }
-
-  }
+  }, [createCategory, inputs]);
 
   return (
     <Container>
       <form>
-
         <h1>Join Room</h1>
 
         <InputOTP inputs={inputs} setInputs={setInputs} />
 
         <Footer>
-          <Button type='button' onClick={setClose}>Cancelar</Button>
+          <Button type="button" onClick={setClose}>
+            Cancelar
+          </Button>
         </Footer>
       </form>
-    </Container >
+    </Container>
   );
 }
---
+++ src/types/error-back.ts
@@ -0,0 +1,5 @@
+export interface BackError {
+  statusCode: number;
+  message: string;
+  error: string;
+}
